7b27049655e54701545e8e9480876a70a3952d46
datpham96 <phamdat22396@gmail.com>
2019-08-26 16:23:52 +0700

    cm

--- resources/views/Email/layout/index.blade.php
+++ resources/views/Email/layout/index.blade.php
@@ -6,6 +6,7 @@
     <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
     <title>Nofification CA</title>
     <link href="{{ asset('/css/style.css') }}" media="all" rel="stylesheet" type="text/css" />
+    
 </head>
 
 <body>
