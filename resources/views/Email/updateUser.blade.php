0a8334dfad82575ef6676ef104c5620321263623
Tiến Đạt <phamdat22396@gmail.com>
2019-08-26 16:17:25 +0700

    cm

---
+++ resources/views/Email/updateUser.blade.php
@@ -0,0 +1,24 @@
+@extends('Email.layout.index')
+
+@section('content')
+<tr>
+	<td class="content-block">
+		<h3>Cập nhật tài khoản</h3>
+	</td>
+</tr>
+<tr>
+	<td class="content-block">
+		Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.
+	</td>
+</tr>
+<tr>
+	<td class="content-block">
+		Please confirm your email address by clicking the link below.
+	</td>
+</tr>
+<tr>
+	<td class="content-block">
+		<img src="https://images.pexels.com/photos/414612/pexels-photo-414612.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" alt="">
+	</td>
+</tr>
+@endsection
