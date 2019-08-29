b22bd28058c3b4047087e2812e2da6f9553ef161
datpham96 <phamdat22396@gmail.com>
2019-08-29 15:19:34 +0700

    cm

--- resources/views/Email/layout.blade.php
+++ resources/views/Email/layout.blade.php
@@ -6,6 +6,9 @@
     <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
     <title>Nofification CA</title>
     <link href="{{ asset('/css/style.css') }}" media="all" rel="stylesheet" type="text/css" />
+    <style>
+        
+    </style>
 </head>
 
 <body>
